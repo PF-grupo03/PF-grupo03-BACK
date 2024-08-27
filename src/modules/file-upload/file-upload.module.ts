a56478216211de352d7360ff2eb8d083d0ac30a1
Lmurillo <leobachy22@gmail.com>
2024-08-27 13:28:41 -0300

    solución problema cloudinary

--- src/modules/file-upload/file-upload.module.ts
+++ src/modules/file-upload/file-upload.module.ts
@@ -1,14 +1,28 @@
+// import { Module } from '@nestjs/common';
+// import { FileUploadService } from './file-upload.service';
+// import { FileUploadController } from './file-upload.controller';
+// import { ProductEntity } from '../products/product.entity';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { CloudinaryConfig } from 'src/config/cloudinary.config';
+// import { FileUploadRepository } from './file-upload.repository';
+
+// @Module({
+//   imports: [TypeOrmModule.forFeature([ProductEntity])],
+//   controllers: [FileUploadController],
+//   providers: [FileUploadService, CloudinaryConfig, FileUploadRepository],
+// })
+// export class FileUploadModule {}
+
 import { Module } from '@nestjs/common';
 import { FileUploadService } from './file-upload.service';
 import { FileUploadController } from './file-upload.controller';
 import { ProductEntity } from '../products/product.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CloudinaryConfig } from 'src/config/cloudinary.config';
 import { FileUploadRepository } from './file-upload.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ProductEntity])],
   controllers: [FileUploadController],
-  providers: [FileUploadService, CloudinaryConfig, FileUploadRepository],
+  providers: [FileUploadService, FileUploadRepository],
 })
 export class FileUploadModule {}
--- src/modules/file-upload/file-upload.repository.ts
+++ src/modules/file-upload/file-upload.repository.ts
@@ -1,17 +1,41 @@
+// import { Injectable } from "@nestjs/common";
+// import { UploadApiResponse, v2 } from "cloudinary";
+// import toStream = require('buffer-to-stream');
+
+// @Injectable()
+// export class FileUploadRepository {
+//   async uploadImage(
+//     file: Express.Multer.File
+// ): Promise<UploadApiResponse> {
+//     return new Promise((resolve, reject) => {
+//       const upload = v2.uploader.upload_stream(
+//         { resource_type: 'auto', folder: 'travel_zone_cloudinary' },
+//         (error, result) => {
+//           if (error) {
+//             reject(error);
+//           } else {
+//             resolve(result);
+//           }
+//         },
+//       );
+//       toStream(file.buffer).pipe(upload);
+//     });
+//   }
+// }
+
 import { Injectable } from "@nestjs/common";
 import { UploadApiResponse, v2 } from "cloudinary";
 import toStream = require('buffer-to-stream');
 
 @Injectable()
 export class FileUploadRepository {
-  async uploadImage(
-    file: Express.Multer.File
-): Promise<UploadApiResponse> {
+  async uploadImage(file: Express.Multer.File): Promise<UploadApiResponse> {
     return new Promise((resolve, reject) => {
       const upload = v2.uploader.upload_stream(
         { resource_type: 'auto', folder: 'travel_zone_cloudinary' },
         (error, result) => {
           if (error) {
+            console.error('Error al subir imagen a Cloudinary:', error);
             reject(error);
           } else {
             resolve(result);
@@ -22,3 +46,4 @@ export class FileUploadRepository {
     });
   }
 }
+
