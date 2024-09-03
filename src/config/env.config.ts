40c14efe97452ec69e9bd8113219043529fdc3d0
Thiago Milanca <thiagomilanca@gmail.com>
2024-09-03 14:32:36 -0300

    false

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -14,7 +14,7 @@ export const JWT_SECRET = process.env.JWT_SECRET;
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 export const GOOGLE_SECRET = process.env.GOOGLE_SECRET;
 
-export const RESTART_SCHEMA = true;
+export const RESTART_SCHEMA = false;
 
 export const MAIL_HOST = process.env.MAIL_HOST;
 export const MAIL_USER = process.env.MAIL_USER;
