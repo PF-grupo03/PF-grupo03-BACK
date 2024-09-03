52b19bc5e21dc1264db20d264678970efaa423f8
Thiago Milanca <thiagomilanca@gmail.com>
2024-09-03 14:13:37 -0300

    corregido el module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersService } from './users.service';
 import { UsersRepository } from './user.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './user.entity';
+import { MailModule } from 'src/mail/mail.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity]), MailModule],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
 })
