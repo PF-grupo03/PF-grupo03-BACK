5394e2172be4d77fdc88dfb6360710ba1ba81660
Camilo Navas <navasochoa@hotmail.com>
2024-09-04 16:24:37 -0500

    port

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -13,6 +13,8 @@ export const JWT_SECRET = process.env.JWT_SECRET;
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 export const GOOGLE_SECRET = process.env.GOOGLE_SECRET;
 
+export const PORT = process.env.PORT;
+
 
 export const MAIL_HOST = process.env.MAIL_HOST;
 export const MAIL_USER = process.env.MAIL_USER;
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { PORT } from './config/env.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,7 +21,7 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  const port = process.env.PORT || 3006;
+  const port = PORT || 3006;
   await app.listen(port);
   console.log(`Server running on port ${port}`);
 }
