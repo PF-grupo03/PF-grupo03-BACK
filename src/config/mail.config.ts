8bd1119a7ea1cd16f46fedcb9c222d645a7c9da7
Thiago Milanca <thiagomilanca@gmail.com>
2024-09-04 23:07:04 -0300

    587

--- src/config/mail.config.ts
+++ src/config/mail.config.ts
@@ -5,13 +5,14 @@ import { Transporter } from 'nodemailer';
 export const mailerConfig: Transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST || 'smtp.gmail.com',
   port: parseInt(process.env.MAIL_PORT, 10) || 587,
-  secure: process.env.MAIL_SECURE === 'false',
+  secure: false,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASSWORD,
   },
   tls: {
     minVersion: 'TLSv1.2',
+    ciphers: 'TLS_AES_256_GCM_SHA384',
     rejectUnauthorized: false,
   },
   logger: true,
