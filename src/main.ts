c07c7a297f66eb6a40f356e427a613d577bce281
Lmurillo <leobachy22@gmail.com>
2024-09-08 21:02:26 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://travelzone-git-develop-grupo03s-projects.vercel.app/']
+    origin: ['http://localhost:3000', 'https://travelzone-git-develop-grupo03s-projects.vercel.app/', 'http://localhost:3006']
   , credentials: true
   });
 
