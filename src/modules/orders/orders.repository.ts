dedf1a028256b280c703f1908c1567c1e9e00e85
Camilo Navas <navasochoa@hotmail.com>
2024-09-08 19:39:26 -0500

    user entity, dto

--- src/modules/orders/orders.repository.ts
+++ src/modules/orders/orders.repository.ts
@@ -153,9 +153,6 @@ export class OrdersRepository {
 
       // return { order: orderWithDates };
 
-        //   const successUrl = 'https://pf-grupo03-back.onrender.com/payment-success';
-        //   const cancelUrl = 'https://pf-grupo03-back.onrender.com/payment-cancel';
-
         const successUrl = 'http://localhost:3006/payment-success';
         const cancelUrl = 'http://localhost:3006/payment-cancel'; 
         
