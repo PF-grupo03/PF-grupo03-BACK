32990c13505909f58b3663fc4407acc0c13f0220
Lmurillo <leobachy22@gmail.com>
2024-09-16 10:28:18 -0300

    login de google completo

--- src/modules/users/user.repository.ts
+++ src/modules/users/user.repository.ts
@@ -401,93 +401,25 @@ export class UsersRepository {
     }
   }
 
-  // async createUserGoogle(userGoogle: any) {
-  //   try {
-  //     const newUser = new UserEntity();
-  //     newUser.email = userGoogle.email;
-  //     newUser.name = `${userGoogle.firstName} ${userGoogle.lastName}`;
-  //     newUser.username = `${userGoogle.firstName}`;
-  //     newUser.dni = this.generateRandomDNI();
-  //     newUser.phone = this.generateRandomPhone();
-  //     newUser.password = this.generateRandomPassword();
-  
-  //     const savedUser = await this.usersRepository.save(newUser);
-  //     return savedUser;
-  //   } catch (error) {
-  //     console.error("Error al crear el usuario:", error);
-  //     throw new InternalServerErrorException('Error al crear el usuario');
-  //   }
-  // }
-
-
-  // async createUserGoogle(userGoogle: any): Promise<UserEntity> {
-  //   try {
-  //     const { email, firstName, lastName, profileImage } = userGoogle;
-
-  //     // Subir la imagen a Cloudinary si está disponible
-  //     let imageProfileUrl = DEFAULT_PROFILE_IMAGE_USER;  // Usa la imagen por defecto en caso de error
-
-  //     if (profileImage) {
-  //       const uploadImageToCloudinary = (): Promise<UploadApiResponse> => {
-  //         return new Promise((resolve, reject) => {
-  //           const uploadStream = cloudinary.uploader.upload_stream(
-  //             { resource_type: 'auto', folder: 'travel_zone_cloudinary' },
-  //             (error, result) => {
-  //               if (error) {
-  //                 reject(error);
-  //               } else {
-  //                 resolve(result);
-  //               }
-  //             }
-  //           );
-  //           toStream(profileImage).pipe(uploadStream);
-  //         });
-  //       };
-
-  //       const uploadResult = await uploadImageToCloudinary();
-  //       imageProfileUrl = uploadResult.secure_url;
-  //     }
-
-  //     const plainPassword = this.generateRandomPassword();
-  //     const hashedPassword = await bcrypt.hash(plainPassword, 10);
-
-  //     // Crear y guardar el nuevo usuario
-  //     const newUser = new UserEntity();
-  //     newUser.email = email;
-  //     newUser.name = `${firstName} ${lastName}`;
-  //     newUser.username = `${firstName}`;
-  //     newUser.dni = this.generateRandomDNI();
-  //     newUser.phone = this.generateRandomPhone();
-  //     newUser.password = hashedPassword;
-  //     newUser.imageProfile = imageProfileUrl;
-
-  //     const savedUser = await this.usersRepository.save(newUser);
-  //     return savedUser;
-  //   } catch (error) {
-  //     console.error("Error al crear el usuario:", error);
-  //     throw new InternalServerErrorException('Error al crear el usuario');
-  //   }
-  // }
-
   async createUserGoogle(userGoogle: any): Promise<UserEntity> {
     try {
       const { email, firstName, lastName, profileImage } = userGoogle;
 
-      // Subir la imagen a Cloudinary si está disponible
-      let imageProfileUrl = DEFAULT_PROFILE_IMAGE_USER;  // Usa la imagen por defecto en caso de error
+      
+      let imageProfileUrl = DEFAULT_PROFILE_IMAGE_USER;  
 
       if (profileImage) {
         try {
-          // Descargar la imagen del enlace proporcionado por Google
+          
           const response = await fetch(profileImage);
           if (!response.ok) {
             throw new BadRequestException('No se pudo descargar la imagen de perfil.');
           }
 
-          // Leer la imagen como buffer
+          
           const imageBuffer = Buffer.from(await response.arrayBuffer());
 
-          // Subir la imagen a Cloudinary
+          
           const uploadImageToCloudinary = (): Promise<UploadApiResponse> => {
             return new Promise((resolve, reject) => {
               const uploadStream = cloudinary.uploader.upload_stream(
@@ -512,11 +444,11 @@ export class UsersRepository {
         }
       }
 
-      // Generar una contraseña aleatoria y hacerle hash
+      
       const plainPassword = this.generateRandomPassword();
       const hashedPassword = await bcrypt.hash(plainPassword, this.saltRounds);
 
-      // Crear y guardar el nuevo usuario
+      
       const newUser = new UserEntity();
       newUser.email = email;
       newUser.name = `${firstName} ${lastName}`;
